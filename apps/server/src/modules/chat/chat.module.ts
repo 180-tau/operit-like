99cb294ff731f5800a6feb86a766775777196885
180-tau <1871974488@qq.com>
2026-08-11 08:14:45 +0800

    feat: import packages module

--- apps/server/src/modules/chat/chat.module.ts
+++ apps/server/src/modules/chat/chat.module.ts
@@ -6,9 +6,10 @@ import { LlmModule } from '../llm/llm.module.js';
 import { CharacterModule } from '../character/character.module.js';
 import { MemoryModule } from '../memory/memory.module.js';
 import { ToolsModule } from '../tools/tools.module.js';
+import { PackagesModule } from '../packages/package.module.js';
 
 @Module({
-  imports: [ConversationModule, LlmModule, CharacterModule, MemoryModule, ToolsModule],
+  imports: [ConversationModule, LlmModule, CharacterModule, MemoryModule, ToolsModule, PackagesModule],
   controllers: [ChatController],
   providers: [ChatService],
 })
